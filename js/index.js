6f66b1fb31847fc0edcfeabd725e6bcf97c5e12a
chander1078 <shekharsaa@gmail.com>
2020-09-08 15:14:21 +0530

    Firstcommit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(".book-apt-btn").click(function() {
+        $(".backdrop-apt-section").css("display", "block");
+    });
+    $(".close-btn").click(function() {
+        $(".backdrop-apt-section").css("display", "none");
+    });
+});
+
